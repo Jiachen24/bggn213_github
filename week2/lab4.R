26087f1c92ca37729392e3f100d3082f9a13ae06
Jiachen <j9fan@ucsd.edu>
2023-12-11 11:09:30 -0800

    add files

---
+++ week2/lab4.R
@@ -0,0 +1,15 @@
+#Jiachen_Fan_Lab_4
+
+x <- 1:50
+
+plot(x)
+
+plot(x, sin(x))
+
+plot(x, sin(x), type = 'l')
+
+plot(x, sin(x), type = 'l', col = 'blue', lwd = 3)
+
+plot(x, sin(x), type = 'l', col = 'blue', lwd = 3, xlab ='x(integer)',)
+
+title(main ='Function')
